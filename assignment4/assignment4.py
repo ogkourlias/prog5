70cf5f5fddbb797d22e27ce7dc23d7cc9ed09a06
ogkourlias <orfeasgkourlias@gmail.com>
2025-10-03 18:34:11 +0200

    Adding docstrings, reformatted assignment4.py with black.

--- assignment4/assignment4.py
+++ assignment4/assignment4.py
@@ -16,17 +16,16 @@ import sys
 import argparse
 from Bio import SeqIO
 import sqlalchemy
-from sqlalchemy import MetaData, Table, Column, Integer, String, VARCHAR
-if sqlalchemy.__version__.startswith('1.4'):
+from sqlalchemy import MetaData, Table, Column, Integer, VARCHAR
+
+if sqlalchemy.__version__.startswith("1.4"):
     from sqlalchemy import create_engine
     from sqlalchemy.engine import make_url, URL
     from sqlalchemy.sql import text
-elif sqlalchemy.__version__.startswith('2'):
+elif sqlalchemy.__version__.startswith("2"):
     from sqlalchemy import create_engine
     from sqlalchemy.url import make_url, URL
     from sqlalchemy.sql import text
-    
-
 
 
 def argparser():
@@ -35,67 +34,93 @@ def argparser():
     parser.add_argument("--input", "-i", default=2, type=str)
     return parser.parse_args()
 
+
 def make_engine():
-    myDB = URL.create(
-    drivername="mariadb",
-    query={"read_default_file": os.path.expanduser("~/.my.cnf")}
+    """MariaDB engine creation"""
+    my_db = URL.create(
+        drivername="mariadb",
+        query={"read_default_file": os.path.expanduser("~/.my.cnf")},
     )
-    engine = create_engine(myDB, echo=False)
+    engine = create_engine(my_db, echo=False)
     return engine
 
-def create_species_table(engine): # Species name, Species acc, Genome size, Number of genes, Number of proteins, TaxDB
+
+def create_species_table(
+    engine,
+):  # Species name, Species acc, Genome size, Number of genes, Number of proteins, TaxDB
+    """Create table with species columns"""
     metadata = MetaData()
     species = Table(
-    "species", metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", VARCHAR(255), nullable=True),
-    Column("acc_id", VARCHAR(255), nullable=True),
-    Column("genome_size", Integer, nullable=True),
-    Column("num_genes", Integer, nullable=True),
-    Column("num_proteins", Integer, nullable=True),
-    Column("taxdb", VARCHAR(255), nullable=True)
-)
+        "species",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("name", VARCHAR(255), nullable=True),
+        Column("acc_id", VARCHAR(255), nullable=True),
+        Column("genome_size", Integer, nullable=True),
+        Column("num_genes", Integer, nullable=True),
+        Column("num_proteins", Integer, nullable=True),
+        Column("taxdb", VARCHAR(255), nullable=True),
+    )
     species.drop(engine, checkfirst=True)
     metadata.create_all(engine)
     return species
 
 
-def create_protein_table(engine):  # Protein ID, Protein Product, Protein Location, Locus Tag, Gene ref, EC number, GO Annotation
+def create_protein_table(
+    engine,
+):  # Protein ID, Protein Product, Protein Location, Locus Tag, Gene ref, EC number, GO Annotation
+    """Create table with protein columns"""
     metadata = MetaData()
     proteins = Table(
-    "proteins", metadata,
-    Column("id", Integer, primary_key=True),
-    Column("name", VARCHAR(255), nullable=True),
-    Column("protein_id", VARCHAR(255), nullable=True),
-    Column("loc", VARCHAR(255), nullable=True),
-    Column("loc_tag", VARCHAR(255), nullable=True),
-    Column("gene_ref", VARCHAR(255), nullable=True),
-    Column("ec_num", VARCHAR(255), nullable=True),
-    Column("go_annot", VARCHAR(255), nullable=True),
-    Column("go_func", VARCHAR(255), nullable=True)
-)
+        "proteins",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("name", VARCHAR(255), nullable=True),
+        Column("protein_id", VARCHAR(255), nullable=True),
+        Column("loc", VARCHAR(255), nullable=True),
+        Column("loc_tag", VARCHAR(255), nullable=True),
+        Column("gene_ref", VARCHAR(255), nullable=True),
+        Column("ec_num", VARCHAR(255), nullable=True),
+        Column("go_annot", VARCHAR(255), nullable=True),
+        Column("go_func", VARCHAR(255), nullable=True),
+    )
     proteins.drop(engine, checkfirst=True)
     metadata.create_all(engine)
     return proteins
 
-def create_link_table(engine):  # Protein ID, Protein Product, Protein Location, Locus Tag, Gene ref, EC number, GO Annotation
+
+def create_link_table(
+    engine,
+):  # Protein ID, Protein Product, Protein Location, Locus Tag, Gene ref, EC number, GO Annotation
+    """Create table linking proteins to species"""
     metadata = MetaData()
     species_protein = Table(
-    "species_protein", metadata,
-    Column("id", Integer, primary_key=True),
-    Column("protein_id", VARCHAR(255), nullable=True),
-    Column("species_id", VARCHAR(255), nullable=True)
-)   
+        "species_protein",
+        metadata,
+        Column("id", Integer, primary_key=True),
+        Column("protein_id", VARCHAR(255), nullable=True),
+        Column("species_id", VARCHAR(255), nullable=True),
+    )
     species_protein.drop(engine, checkfirst=True)
     metadata.create_all(engine)
     return species_protein
 
+
 def process_batch(connection, table, insertion_list):
+    """Insert list of dictionaries into specified table"""
     connection.execute(table.insert(), insertion_list)
 
-def parse_genbank(ref_path, engine, species_table, protein_table, link_table, batch_size = 5000):
-    species_buffer = []  # Species name, Species acc, Genome size, Number of genes, Number of proteins, TaxDB
-    protein_buffer = []  # Protein ID, Protein Product, Protein Location, Locus Tag, Gene ref, EC number, GO Annotation
+
+def parse_genbank(
+    ref_path, engine, species_table, protein_table, link_table, batch_size=5000
+):
+    """Go through genbank format file and store species and proteins into database"""
+    species_buffer = (
+        []
+    )  # Species name, Species acc, Genome size, Number of genes, Number of proteins, TaxDB
+    protein_buffer = (
+        []
+    )  # Protein ID, Protein Product, Protein Location, Locus Tag, Gene ref, EC numb, GO Annotation
     link_buffer = []
 
     for record in SeqIO.parse(ref_path, format="genbank"):
@@ -111,25 +136,21 @@ def parse_genbank(ref_path, engine, species_table, protein_table, link_table, ba
             "genome_size": genome_size,
             "name": species_name,
             "num_genes": 0,
-            "num_proteins": 0
+            "num_proteins": 0,
         }
 
         # Find gene specifics in features
         for feature in features:
             ftype = feature.type
             qualifiers = feature.qualifiers
-            
             if ftype == "source":
-                record_dict["taxdb"] = qualifiers.get("db_xref")# TaxDB Id
-            
+                record_dict["taxdb"] = qualifiers.get("db_xref")  # TaxDB Id
             elif ftype == "gene":
                 record_dict["num_genes"] += 1
-
             elif ftype == "CDS":
                 record_dict["num_proteins"] += 1
                 protein_id = qualifiers.get("protein_id", [None])[0]
-                link_dict = {"protein_id" : protein_id,
-                            "species_id" : record_id}
+                link_dict = {"protein_id": protein_id, "species_id": record_id}
                 protein_dict = {
                     "protein_id": protein_id,
                     "name": qualifiers.get("product", [""])[0],
@@ -138,7 +159,7 @@ def parse_genbank(ref_path, engine, species_table, protein_table, link_table, ba
                     "gene_ref": qualifiers.get("inference", [""])[0].split(":")[-1],
                     "go_annot": qualifiers.get("GO_process", [None])[0],
                     "go_func": qualifiers.get("GO_function", [None])[0],
-                    "ec_num": qualifiers.get("EC_number", [None])[0]
+                    "ec_num": qualifiers.get("EC_number", [None])[0],
                 }
                 protein_buffer.append(protein_dict)
                 link_buffer.append(link_dict)
@@ -147,7 +168,6 @@ def parse_genbank(ref_path, engine, species_table, protein_table, link_table, ba
                 if len(protein_buffer) >= batch_size:
                     process_batch(engine, protein_table, protein_buffer)
                     protein_buffer.clear()
-                    
                 # Insert into table and flush buffer list
                 if len(link_buffer) >= batch_size:
                     process_batch(engine, link_table, link_buffer)
@@ -170,6 +190,7 @@ def parse_genbank(ref_path, engine, species_table, protein_table, link_table, ba
     if link_buffer:
         process_batch(engine, link_table, link_buffer)
 
+
 # MAIN
 def main(args):
     """Main function"""
@@ -178,7 +199,9 @@ def main(args):
     species_table = create_species_table(engine)
     protein_table = create_protein_table(engine)
     link_table = create_link_table(engine)
-    parse_genbank(args.input, engine, species_table, protein_table, link_table, batch_size=5000)
+    parse_genbank(
+        args.input, engine, species_table, protein_table, link_table, batch_size=5000
+    )
     # FINISH
     return 0
 
